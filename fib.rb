e43f079e67da2ad94096d54f20f88682029041ee
David Taylor <taylor.david.ray@gmail.com>
2015-02-06 11:31:45 -0800

    Working recursive fibonacci

--- fib.rb
+++ fib.rb
@@ -1,5 +1,4 @@
 def fib(index)
-  #ary = Array.new(index) { |i| (i==0 || i==1) ? 1 : ary[i-1] + ary[i-2] }
   ary = []
   (index).times { |i| ary << ((i==0 || i==1) ? 1 : ary[i-2] + ary[i-1]) }
   ary
@@ -8,26 +7,22 @@ end
 
 print fib(3)
 puts
-=begin
 print fib(4)
 puts
 print fib(5)
 puts
-=end
+
 
 def fib_rec(index)
-  ary = []
-  if index <= 2
-  	ary = [1]
-  else
-  	ary = ary + (fib_rec(index-2) + fib_rec(index-1))
-  end
-  ary
+  return [1] if index <= 1
+  return [1, 1] if index == 2
+  ary = fib_rec(index-1)
+  ary + [ ary[0]+ary[1] ]
 end
 
 print fib_rec(3)
 puts
-#print fib(4)
-#puts
-#print fib(5)
-#puts
+print fib(4)
+puts
+print fib(5)
+puts
